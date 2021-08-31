cc6d0e20b468d57b50789a8a62d00306cdde226d
Moe T <78877990+mrtah76@users.noreply.github.com>
2021-08-31 00:01:57 -0700

    final revision

--- StarterCode/config.py
+++ StarterCode/config.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "20273c3970a10a3cca14e70a8436a8fd"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyB0Q85by2unEwffZVWvtoZo-UFqU3pdxKw"
+g_key = ""
